94dc8dee91428c28552e9dee1f174da64756171a
Megan Slawson <meg.slawson@gmail.com>
2020-04-27 03:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-let holiday = "Halloween"
-let name = "Megan"
+let holiday = "Independence Day"
+let name = "you"
+let number = 20
+let mother = "Mother's Day"
 
 function happyHolidays(string) {
  return "Happy holidays!"
 }
+happyHolidays()
+
+function happyCustomHolidayTo(name) {
+ return `Happy holidays,${name}!`
+}
 happyHolidaysTo()
 
-function happyCustomHolidayTo(holiday, name) {
- return 'Happy ${holiday}, ${name}!'
+function happyHolidaysTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
 }
+happyHolidaysTo()
+
+function holidayCountdown(mother,number){
+  return `It's ${number} days until ${mother}!`
+}
+holidayCountdown()
