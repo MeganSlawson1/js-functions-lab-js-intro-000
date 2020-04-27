4ea27fc21e6f6473d558ac9cd3ae169303791f9b
Megan Slawson <meg.slawson@gmail.com>
2020-04-27 03:26:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ var father = "Father's Day"
 function happyHolidays(string) {
  return "Happy holidays!"
 }
-happyHolidays()
+
 
 function happyCustomHolidayTo(name) {
  return `Happy holidays, ${name}!`
 }
-happyHolidaysTo()
+
 
 function happyHolidaysTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
 }
-happyHolidaysTo()
 
 function holidayCountdown(mother,number){
   return `It's ${days} days until ${mother}!`
 }
-holidayCountdown()
